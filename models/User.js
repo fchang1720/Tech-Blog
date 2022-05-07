d54c9b5a9f71bc375743bd1cf2fea067ebdfe67e
Crucix <fchang1720@gmail.com>
2022-05-07 13:48:31 -0500

    finished setting up models

--- models/User.js
+++ models/User.js
@@ -20,11 +20,6 @@ User.init(
             allowNull: false,
         },
 
-        email:{
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-
         password:{
             type: DataTypes.STRING,
             allowNull: false,
---
+++ models/index.js
@@ -0,0 +1,21 @@
+const User = require("./User.js")
+const Post = require("./Post.js")
+const Comment = require("./Comment.js")
+
+Post.belongsTo(User,{
+    foreignKey: "user_id",
+});
+
+User.hasMany(Post, {
+    foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+})
+
+module.exports = {User, Post, Comment};
