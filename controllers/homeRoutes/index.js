330937a65c9b28c7d2968c8d75f2eff93a5ec737
Crucix <fchang1720@gmail.com>
2022-05-07 13:10:37 -0500

    added basic dashboard route

--- controllers/homeRoutes/index.js
+++ controllers/homeRoutes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const loginRoutes = require("./login.js");
+const dashBoardRoutes = require("./dashboard.js")
 
 router.use("/login",loginRoutes)
+router.use("/dashBoard",dashBoardRoutes)
 
 router.get("/",(req,res)=>{
 
